2273253eedae9f032e4f8ac528b0080329f5ea34
Davi Sampaio <davicav@gmail.com>
2013-03-15 22:31:01 -0300

    Alteracoes para uso da media das distancias no calculo da similaridade

--- source/entro.py
+++ source/entro.py
@@ -6,9 +6,9 @@ import sys
 
 def dist(i, j, m_info):
     """
-    :param i: list of features of 'i'
-    :param j: list of features of 'j'
-    :param m_info: dictionary with the matrix information about min and max values of all features
+    :param i: i's feature list
+    :param j: j's feature list
+    :param m_info:
     :return: distance between the data points 'i' and 'j'
     :rtype : float
     """
@@ -24,27 +24,24 @@ def dist(i, j, m_info):
     return math.sqrt(dist)
 
 
-def sim(i, j, m_info):
+def sim(i, j, dm, dm_avg):
     """
-    :param i: list of features of 'i'
-    :param j: list of features of 'j'
-    :param m_info: dictionary with the matrix information about min and max values of all features
+    :param i: point i
+    :param j: point j
+    :param dm: matrix with the distance between the points
+    :param dm_avg: average distance between data points computed over the entire data set
     :return: similarity of the data points 'i' and 'j'
     :rtype : float
     """
-    alfa = -math.log(0.5) / 0.3  # missing the " / avg(dist)"
+    alfa = -math.log(0.5) / dm_avg
 
-    return math.exp(-alfa * dist(i, j, m_info))
+    return math.exp(-alfa * dm[i][j])
 
 
-# input: list of a list of features of attributes
-# [[f1,f2,f3],[f1,f2,f3],[f1,f2,f3],[f1,f2,f3]]
-#
-# return: dictionary with information about the matrix
 def get_info(m):
     """
     :param m: list of attributes with a list of features
-    :return: dictionary with the matrix information about min and max values of all features
+    :return: dictionary with the min and max values to every feature
     :rtype : dict
     """
     len_rows = len(m)
@@ -74,36 +71,47 @@ def get_distance_matrix(m):
     """
     :param m: list of attributes with a list of features
     :return: matrix with the distances between the attributes along all the features direction
-    :rtype : list
+    :rtype : list, float
     """
     # init the distance matrix
     dm = [[0.0 for j in range(len(m))] for i in range(len(m))]
 
+    dm_sum_total = 0.0
+    dm_count = 0
     m_info = get_info(m)
     # calculate the distances while i < j. dist(i , j) = dist(j, i)
     for i in range(len(m)):
         for j in range(i + 1, len(m)):
             dm[i][j] = dist(m[i], m[j], m_info)
+            dm[j][i] = dm[i][j]
 
-    return dm
+            # calculate the average sum between the attributes
+            dm_sum_total += dm[i][j]
+            dm_count += 1
+
+    dm_avg = dm_sum_total / dm_count
+
+    return dm, dm_avg
 
 
 def entropy(m):
     """
-    :rtype : float
-    :param m: list of attributes with a list of features 
+    :param m: list of attributes with a list of features
     :return: entropy of the attributes set
+    :rtype : float
     """
     m_info = get_info(m)
     len_rows = m_info['len_rows']
-    distance_matrix = get_distance_matrix(m)
+    distance_matrix, dm_avg = get_distance_matrix(m)
     entropy = 0.0
 
     for i in range(len_rows):
         for j in range(i + 1, len_rows):
-            if m[i] != m[j]:
-                sim_ij = sim(m[i], m[j], m_info)
-                entropy += (sim_ij * math.log(sim_ij) + (1 - sim_ij) * math.log(1 - sim_ij))
+            sim_ij = sim(i, j, distance_matrix, dm_avg)
+            if sim_ij == 1:  # i and j are equal
+                continue
+
+            entropy += (sim_ij * math.log(sim_ij) + (1 - sim_ij) * math.log(1 - sim_ij))
 
     return -entropy
 
